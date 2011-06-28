2f6f97ef293acdb24a91a37644f0311dba540e5b
james.lamberg@gmail.com <james.lamberg@gmail.com@6cd34ec6-681c-2c1c-7736-812b3a1fb6f0>
2011-06-28 23:59:45 +0000

    Autoversioning commit:  a non-deltaV client made a change to /trunk/main.m

---
+++ trunk/main.m
@@ -0,0 +1,28 @@
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+//  main.m
+//  Gas Guide iOS App
+//
+//	Original Modification by James Lamberg 21Nov09
+//	Updated by James Lamberg 19Dec09 v1.0
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+// This work is licensed under the Creative Commons
+// Attribution-NonCommercial-ShareAlike 3.0 Unported License.
+// To view a copy of this license, visit: 
+// creativecommons.org/licenses/by-nc-sa/3.0/
+// or send a letter to: 
+// Creative Commons
+// 444 Castro Street, Suite 900
+// Mountain View, California, 94041, USA.
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
