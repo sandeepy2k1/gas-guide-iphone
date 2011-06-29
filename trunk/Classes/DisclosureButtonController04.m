7b387a77f28f7ce2173ad30cc95bfdd499e7ad29
james.lamberg@gmail.com <james.lamberg@gmail.com@6cd34ec6-681c-2c1c-7736-812b3a1fb6f0>
2011-06-29 00:00:27 +0000

    Autoversioning commit:  a non-deltaV client made a change to /trunk/Classes/DisclosureButtonController04.m

---
+++ trunk/Classes/DisclosureButtonController04.m
@@ -0,0 +1,139 @@
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+//  DisclosureButtonController.m
+//  Gas Guide iOS App
+//
+//	Original Modification by James Lamberg 21Nov09
+//	Updated by James Lamberg 19Dec09 v1.0
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+#import "NavAppDelegate.h"
+#import "DisclosureButtonController04.h"
+#import "DisclosureDetailController.h"
+#import "ImageViewController.h"
+
+@implementation DisclosureButtonController04
+@synthesize list;
+- (void)viewDidLoad {	// * * * * Nerve Blockade * * * *
+    NSArray *array = [[NSArray alloc] initWithObjects:
+					  @"General Principles",		// Option 0
+					  @"Ankle Block",				// Option 1
+                      @"Axillary Block",			// Option 2
+                      @"Bier Block",                // Option 3
+					  @"Fascia Iliaca Block",		// Option 4					  
+					  @"Femoral Block",				// Option 5
+					  @"Infraclavicular Block",		// Option 6
+					  @"Interscalene Block",		// Option 7
+					  @"Neuraxial Antithrombotics",	// Option 8
+					  @"Neuraxial Epidural Block",	// Option 9
+					  @"Neuraxial Spinal Block",	// Option 10
+					  @"Popliteal Block",			// Option 11
+					  @"Sciatic Block",				// Option 12
+                      nil];
+    self.list = array;
+    [array release];
+    [super viewDidLoad];
+}
+- (void)viewDidUnload {
+    self.list = nil;
+    [childController release];
+    childController = nil;
+}
+- (void)dealloc {
+    [list release];
+    [childController release];
+    [super dealloc];
+}
+#pragma mark -
+#pragma mark Table Data Source Methods
+- (NSInteger)tableView:(UITableView *)tableView 
+ numberOfRowsInSection:(NSInteger)section {
+    return [list count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView 
+         cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    static NSString * DisclosureButtonCellIdentifier = 
+    @"DisclosureButtonCellIdentifier";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier: 
+                             DisclosureButtonCellIdentifier];
+    if (cell == nil) {
+        cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault 
+                                       reuseIdentifier: DisclosureButtonCellIdentifier]
+                autorelease];
+    }
+    NSUInteger row = [indexPath row];
+    NSString *rowString = [list objectAtIndex:row];
+    cell.textLabel.text = rowString;
+    cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    [rowString release];
+    return cell;
+}
+
+#pragma mark -
+#pragma mark Table Delegate Methods
+
+
+- (void)tableView:(UITableView *)tableView 
+didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+	if (childController == nil)
+		childController = [[DisclosureDetailController alloc] 
+				   initWithNibName:@"DisclosureDetail" bundle:nil];
+	
+	childController.title = @"Disclosure Button Pressed";
+	NSUInteger row = [indexPath row];
+
+	NSString *selectedMovie = [list objectAtIndex:row];
+
+	NSString *detailMessage  = [[NSString alloc] 
+							initWithFormat:@"Default Text for %@.\n\nUse top left arrow to navigate back", 
+							selectedMovie];
+	
+	if (row == 0) // Option 0
+		detailMessage = @"* Sample Text *\nLine 1\nLine 2\n--------------------------------------------------\nLine 3";
+	if (row == 1) // Option 1
+		detailMessage = @"* Sample Text *\nLine 1\nLine 2\n--------------------------------------------------\nLine 3";
+	if (row == 2) // Option 2
+		detailMessage = @"* Sample Text *\nLine 1\nLine 2\n--------------------------------------------------\nLine 3";
+	if (row == 3) // Option 3
+		detailMessage = @"* Sample Text *\nLine 1\nLine 2\n--------------------------------------------------\nLine 3";
+	if (row == 4) // Option 4
+		detailMessage = @"* Sample Text *\nLine 1\nLine 2\n--------------------------------------------------\nLine 3";
+	if (row == 5) // Option 5
+		detailMessage = @"* Sample Text *\nLine 1\nLine 2\n--------------------------------------------------\nLine 3";
+	if (row == 6) // Option 6
+		detailMessage = @"* Sample Text *\nLine 1\nLine 2\n--------------------------------------------------\nLine 3";
+	if (row == 7) // Option 7
+		detailMessage = @"* Sample Text *\nLine 1\nLine 2\n--------------------------------------------------\nLine 3";
+	if (row == 8) // Option 8
+		detailMessage = @"* Sample Text *\nLine 1\nLine 2\n--------------------------------------------------\nLine 3";
+	if (row == 9) // Option 9
+		detailMessage = @"* Sample Text *\nLine 1\nLine 2\n--------------------------------------------------\nLine 3";
+	if (row == 10) // Option 10
+		detailMessage = @"* Sample Text *\nLine 1\nLine 2\n--------------------------------------------------\nLine 3";
+	if (row == 11) // Option 11
+		detailMessage = @"* Sample Text *\nLine 1\nLine 2\n--------------------------------------------------\nLine 3";
+	if (row == 12) // Option 12
+		detailMessage = @"* Sample Text *\nLine 1\nLine 2\n--------------------------------------------------\nLine 3";
+	
+	childController.message = detailMessage;
+	childController.title = selectedMovie;
+	[detailMessage release];
+
+	[self.navigationController pushViewController:childController
+									 animated:YES];
+}
+
+@end
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+// This work is licensed under the Creative Commons
+// Attribution-NonCommercial-ShareAlike 3.0 Unported License.
+// To view a copy of this license, visit: 
+// creativecommons.org/licenses/by-nc-sa/3.0/
+// or send a letter to: 
+// Creative Commons
+// 444 Castro Street, Suite 900
+// Mountain View, California, 94041, USA.
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
