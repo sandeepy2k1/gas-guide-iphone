41ada8256488a5e20234034d51a1ac305e7e4903
james.lamberg@gmail.com <james.lamberg@gmail.com@6cd34ec6-681c-2c1c-7736-812b3a1fb6f0>
2011-06-29 00:00:52 +0000

    Autoversioning commit:  a non-deltaV client made a change to /trunk/Classes/SecondLevelViewController.h

---
+++ trunk/Classes/SecondLevelViewController.h
@@ -0,0 +1,26 @@
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+//  SecondLevelViewController.h
+//  Gas Guide iOS App
+//
+//	Original Modification by James Lamberg 21Nov09
+//	Updated by James Lamberg 19Dec09 v1.0
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+#import <Foundation/Foundation.h>
+
+@interface SecondLevelViewController : UITableViewController {
+    UIImage    *rowImage;
+}
+@property (nonatomic, retain) UIImage *rowImage;
+@end
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+// This work is licensed under the Creative Commons
+// Attribution-NonCommercial-ShareAlike 3.0 Unported License.
+// To view a copy of this license, visit: 
+// creativecommons.org/licenses/by-nc-sa/3.0/
+// or send a letter to: 
+// Creative Commons
+// 444 Castro Street, Suite 900
+// Mountain View, California, 94041, USA.
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
