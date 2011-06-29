8b5870e4f09e4064f2b690fa5c057af1cf849a53
james.lamberg@gmail.com <james.lamberg@gmail.com@6cd34ec6-681c-2c1c-7736-812b3a1fb6f0>
2011-06-29 00:00:26 +0000

    Autoversioning commit:  a non-deltaV client made a change to /trunk/Classes/DisclosureButtonController04.h

---
+++ trunk/Classes/DisclosureButtonController04.h
@@ -0,0 +1,30 @@
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+//  DisclosureButtonController.h
+//  Gas Guide iOS App
+//
+//	Original Modification by James Lamberg 21Nov09
+//	Updated by James Lamberg 19Dec09 v1.0
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+#import <Foundation/Foundation.h>
+#import "SecondLevelViewController.h"
+
+@class DisclosureDetailController;
+
+@interface DisclosureButtonController04 : SecondLevelViewController {
+    NSArray *list;
+    DisclosureDetailController *childController;
+}
+@property (nonatomic, retain) NSArray *list;
+@end
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+// This work is licensed under the Creative Commons
+// Attribution-NonCommercial-ShareAlike 3.0 Unported License.
+// To view a copy of this license, visit: 
+// creativecommons.org/licenses/by-nc-sa/3.0/
+// or send a letter to: 
+// Creative Commons
+// 444 Castro Street, Suite 900
+// Mountain View, California, 94041, USA.
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
