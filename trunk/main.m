cbf24a637c4fa0374b9f6f3301cd8894c0019e49
james.lamberg@gmail.com <james.lamberg@gmail.com@6cd34ec6-681c-2c1c-7736-812b3a1fb6f0>
2011-06-29 00:05:32 +0000

    Autoversioning commit:  a non-deltaV client made a change to /trunk/main.m

--- trunk/main.m
+++ trunk/main.m
@@ -19,8 +19,20 @@ int main(int argc, char *argv[]) {
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 // This work is licensed under the Creative Commons
 // Attribution-NonCommercial-ShareAlike 3.0 Unported License.
+//
+// Attribution — You must attribute the work in the manner
+// specified by the author or licensor (but not in any way
+// that suggests that they endorse you or your use of the work)
+//
+// Noncommercial — You may not use this work for
+// commercial purposes
+//
+// Share Alike — If you alter, transform, or build upon this
+// work, you may distribute the resulting work only under
+// the same or similar license to this one
+// 
 // To view a copy of this license, visit: 
-// creativecommons.org/licenses/by-nc-sa/3.0/
+// http://creativecommons.org/licenses/by-nc-sa/3.0/
 // or send a letter to: 
 // Creative Commons
 // 444 Castro Street, Suite 900
