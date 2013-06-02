e4320653dd509ff7ec0267fd6d63116b38a5d73b
james.lamberg@gmail.com <james.lamberg@gmail.com@6cd34ec6-681c-2c1c-7736-812b3a1fb6f0>
2013-06-02 19:05:42 +0000

    Autoversioning commit:  a non-deltaV client made a change to /trunk/App_Files/DisclosureButtonController06.h

---
+++ App_Files/DisclosureButtonController06.h
@@ -0,0 +1,43 @@
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+//  DisclosureButtonController.h
+//  Gas Guide iOS App
+//  Open Source Release 2013
+//
+//  Original Modification by James Lamberg 21Nov09
+//  Updated by James Lamberg 19Dec09 v1.0 (First Release)
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+#import <Foundation/Foundation.h>
+#import "SecondLevelViewController.h"
+
+@class DisclosureDetailController;
+
+@interface DisclosureButtonController06 : SecondLevelViewController {
+    NSArray *list;
+    DisclosureDetailController *childController;
+}
+@property (nonatomic, retain) NSArray *list;
+@end
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+// This work is licensed under the Creative Commons
+// Attribution-NonCommercial-ShareAlike 3.0 Unported License.
+//
+// Attribution: You must attribute the work in the manner
+// specified by the author or licensor (but not in any way
+// that suggests that they endorse you or your use of the work)
+//
+// Noncommercial: You may not use this work for
+// commercial purposes
+//
+// Share Alike: If you alter, transform, or build upon this
+// work, you may distribute the resulting work only under
+// the same or similar license to this one
+//
+// To view a copy of this license, visit:
+// http://creativecommons.org/licenses/by-nc-sa/3.0/
+// or send a letter to:
+// Creative Commons
+// 444 Castro Street, Suite 900
+// Mountain View, California, 94041, USA.
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
